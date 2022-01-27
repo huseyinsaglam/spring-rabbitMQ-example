32c9da66d2f9ba3f0dde3e9602285ba8d097abe7
ETIYACORP\huseyin.saglam <huseyin.saglam@etiya.com>
2022-01-27 10:56:55 +0300

    Initial commit

---
+++ src/main/java/com/example/springrabbitMQexample/SpringRabbitMqExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.springrabbitMQexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRabbitMqExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRabbitMqExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springrabbitMQexample/SpringRabbitMqExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springrabbitMQexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRabbitMqExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
